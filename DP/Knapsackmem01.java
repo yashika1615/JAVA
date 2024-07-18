5923027f4dadc17af66f07ccd5bdcc371f396a1d
Yashika1615 <katariay22@gmail.com>
2024-07-18 11:33:34 +0530

    strings

--- DP/Knapsackmem01.java
+++ DP/Knapsackmem01.java
@@ -3,34 +3,33 @@ public class Knapsackmem01 {
         if (W == 0 || n == 0) {
             return 0;
         }
-        if (dp[n][w] != -1)
-            return dp[n][w];
+        if (dp[n][W] != -1)
+            return dp[n][W];
 
-        if (wt <= w) {
-            int ans1 = val[n - 1] + Knapsackmem(val, wt, w - wt, n - 1, dp);
+        if (wt[n - 1] <= W) {
+            int ans1 = val[n - 1] + Knapsackmem(val, wt, W - wt[n - 1], n - 1, dp);
             int ans2 = Knapsackmem(val, wt, W, n - 1, dp);
-            dp[n][w] = Math.max(ans1, ans2);
+            dp[n][W] = Math.max(ans1, ans2);
         } else {
-            dp[n][w] = Knapsackmem(val, wt, w, n - 1, dp);
-            return dp[n][w];
+            dp[n][W] = Knapsackmem(val, wt, W, n - 1, dp);
         }
+        return dp[n][W];
     }
 
     public static void main(String[] args) {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
-        int w = 7;
+        int W = 7;
 
         // create a 2-d dp array
-        int dp[][] = new int[val.length + 1][w + 1];
+        int dp[][] = new int[val.length + 1][W + 1];
         // initialize dp array
         for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp.length; j++) {
+            for (int j = 0; j < dp[i].length; j++) {
                 // fill the dp array
-                dp[i][i] = -1;
+                dp[i][j] = -1;
             }
         }
-        System.out.println(Knapsackmem(val, wt, w, val.length, dp));
+        System.out.println(Knapsackmem(val, wt, W, val.length, dp));
     }
-
 }
--- LINKED LIST/meregell.java
+++
@@ -1,5 +0,0 @@
-//package LINKED LIST;
-
-public class meregell {
-    
-}
